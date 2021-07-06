e556615c646cc22eda2ae5cd2597f8113f53b226
Pratik Talele <pratiktalele22@gmail.com>
2021-07-06 10:59:16 +0530

    newOne

--- helloworld.java
+++ helloworld.java
@@ -2,8 +2,6 @@ package first;
 
 public class helloworld {
  public static void main(String[] args) {
-	System.out.println("hello world");
-	 System.out.println("hello i am modified");
-	 System.out.println(" i am modified by pratik");
+	System.out.println("newOne branch");
 }
 }
